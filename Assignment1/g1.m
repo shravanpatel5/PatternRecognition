bbccac2ea3fe62638126e5b52a96c47fdf29b144
shravanpatel5 <shravanpatel5@gmail.com>
2016-09-10 02:18:15 +0530

    all classifiers completed

--- Assignment1/g1.m
+++ Assignment1/g1.m
@@ -1,3 +1,3 @@
-function val = g1(x,mean,var,prob_c)
-    val = (myMult(myTrans(mean),x)/var) - (myMult(myTrans(mean),mean)/(2*var)) + log(prob_c);
+function val = g1(x,w1,w2)
+    val = myMult(w1,x) + w2;
 end
--- Assignment1/main.m
+++ Assignment1/main.m
@@ -104,6 +104,90 @@ function main(folder,numofclasses,classifier)
                     %22
                 end
             end
+        elseif(classifier==4)
+            cov = (cov1+cov2)/2;
+            cov = diag(diag(cov));
+            sigma=(cov(1,1)+cov(2,2))/2;
+            cov(1,1)=sigma;
+            cov(2,2)=sigma;
+            w11 = myTrans(mean1)/sigma;
+            w12 = (myMult(myTrans(mean1),mean1)/(((-2)*sigma*sigma))) + log(probc1) ;
+            w21 = myTrans(mean2)/sigma;
+            w22 = (myMult(myTrans(mean2),mean2)/(((-2)*sigma*sigma))) + log(probc2) ;
+            for i = 1:length(T1)
+                x = myTrans(T1(i,:)); 
+                x1=g1(x,w11,w12);
+                x2=g1(x,w21,w22);
+                if(x1>x2)
+                    %11
+                else
+                    12
+                end
+            end
+            for i = 1:length(T2)
+                x = myTrans(T2(i,:)); 
+                x1=g1(x,w11,w12);
+                x2=g1(x,w21,w22);
+                if(x1>x2)
+                    21
+                else
+                    %22
+                end
+            end
+        elseif(classifier==5)
+            cov = (cov1+cov2)/2;
+            cov = diag(diag(cov));
+            w11 = myTrans(myMult(myInv(cov),mean1));
+            w12 = - (0.5*myMult(myMult(myTrans(mean1),myInv(cov)),mean1)) + log(probc1);
+            w21 = myTrans(myMult(myInv(cov),mean2));
+            w22 = - (0.5*myMult(myMult(myTrans(mean2),myInv(cov)),mean2)) + log(probc2);
+            for i = 1:length(T1)
+                x = myTrans(T1(i,:)); 
+                x1=g2(x,w11,w12);
+                x2=g2(x,w21,w22);
+                if(x1>x2)
+                    %11
+                else
+                    12
+                end
+            end
+            for i = 1:length(T2)
+                x = myTrans(T2(i,:)); 
+                x1=g2(x,w11,w12);
+                x2=g2(x,w21,w22);
+                if(x1>x2)
+                    21
+                else
+                    %22
+                end
+            end
+        elseif(classifier==6)
+            cov1=diag(diag(cov1));
+            cov2=diag(diag(cov2));
+            w11 = myMult(myTrans(mean1),myInv(cov1));
+            w12 = - (0.5*myMult(myMult(myTrans(mean1),myInv(cov1)),mean1)) + log(probc1) - (0.5*log(abs(myMod(cov1))));
+            w21 = myMult(myTrans(mean2),myInv(cov2));
+            w22 = - (0.5*myMult(myMult(myTrans(mean2),myInv(cov2)),mean2)) + log(probc2) - (0.5*log(abs(myMod(cov2))));
+            for i = 1:length(T1)
+                x = myTrans(T1(i,:)); 
+                x1=g3(x,cov1,w11,w12);
+                x2=g3(x,cov2,w21,w22);
+                if(x1>x2)
+                    %11
+                else
+                    12
+                end
+            end
+            for i = 1:length(T2)
+                x = myTrans(T2(i,:)); 
+                x1=g3(x,cov1,w11,w12);
+                x2=g3(x,cov2,w21,w22);
+                if(x1>x2)
+                    21
+                else
+                    %22
+                end
+            end
         end
     else
         len=len1+len2+len3;
@@ -256,11 +340,154 @@ function main(folder,numofclasses,classifier)
                     %33
                 end
             end
+        elseif(classifier==4)
+            cov = (cov1+cov2+cov3)/3;
+            cov = diag(diag(cov));
+            sigma=(cov(1,1)+cov(2,2))/2;
+            cov(1,1)=sigma;
+            cov(2,2)=sigma;
+            w11 = myTrans(mean1)/sigma;
+            w12 = (myMult(myTrans(mean1),mean1)/(((-2)*sigma*sigma))) + log(probc1) ;
+            w21 = myTrans(mean2)/sigma;
+            w22 = (myMult(myTrans(mean2),mean2)/(((-2)*sigma*sigma))) + log(probc2) ;
+            w31 = myTrans(mean3)/sigma;
+            w32 = (myMult(myTrans(mean3),mean3)/(((-2)*sigma*sigma))) + log(probc3) ;
+            for i = 1:length(T1)
+                x = myTrans(T1(i,:)); 
+                x1=g1(x,w11,w12);
+                x2=g1(x,w21,w22);
+                x3=g1(x,w31,w32);
+                if(x1>x2 && x1>x3)
+                    %11
+                elseif(x2>x1 && x2>x3)
+                    12
+                elseif(x3>x1 && x3>x2)
+                    13
+                end
+            end
+            for i = 1:length(T2)
+                x = myTrans(T2(i,:)); 
+                x1=g1(x,w11,w12);
+                x2=g1(x,w21,w22);
+                x3=g1(x,w31,w32);
+                if(x1>x2 && x1>x3)
+                    21
+                elseif(x2>x1 && x2>x3)
+                    %22
+                elseif(x3>x1 && x3>x2)
+                    23
+                end
+            end
+            for i = 1:length(T3)
+                x = myTrans(T3(i,:)); 
+                x1=g1(x,w11,w12);
+                x2=g1(x,w21,w22);
+                x3=g1(x,w31,w32);
+                if(x1>x2 && x1>x3)
+                    31
+                elseif(x2>x1 && x2>x3)
+                    32
+                elseif(x3>x1 && x3>x2)
+                    %33
+                end
+            end
+        elseif(classifier==5)
+            cov = (cov1+cov2+cov3)/2;
+            cov=diag(diag(cov));
+            w11 = myTrans(myMult(myInv(cov),mean1));
+            w12 = - (0.5*myMult(myMult(myTrans(mean1),myInv(cov)),mean1)) + log(probc1);
+            w21 = myTrans(myMult(myInv(cov),mean2));
+            w22 = - (0.5*myMult(myMult(myTrans(mean2),myInv(cov)),mean2)) + log(probc2);
+            w31 = myTrans(myMult(myInv(cov),mean3));
+            w32 = - (0.5*myMult(myMult(myTrans(mean3),myInv(cov)),mean3)) + log(probc3);
+            for i = 1:length(T1)
+                x = myTrans(T1(i,:)); 
+                x1=g2(x,w11,w12);
+                x2=g2(x,w21,w22);
+                x3=g2(x,w31,w32);
+                if(x1>x2 && x1>x3)
+                    %11
+                elseif(x2>x1 && x2>x3)
+                    12
+                elseif(x3>x1 && x3>x2)
+                    13
+                end
+            end
+            for i = 1:length(T2)
+                x = myTrans(T2(i,:)); 
+                x1=g2(x,w11,w12);
+                x2=g2(x,w21,w22);
+                x3=g2(x,w31,w32);
+                if(x1>x2 && x1>x3)
+                    21
+                elseif(x2>x1 && x2>x3)
+                    %22
+                elseif(x3>x1 && x3>x2)
+                    23
+                end
+            end
+            for i = 1:length(T3)
+                x = myTrans(T3(i,:)); 
+                x1=g2(x,w11,w12);
+                x2=g2(x,w21,w22);
+                x3=g2(x,w31,w32);
+                if(x1>x2 && x1>x3)
+                    31
+                elseif(x2>x1 && x2>x3)
+                    32
+                elseif(x3>x1 && x3>x2)
+                    %33
+                end
+            end
+        elseif(classifier==6)
+            cov1=diag(diag(cov1));
+            cov2=diag(diag(cov2));
+            cov3=diag(diag(cov3));
+            w11 = myMult(myTrans(mean1),myInv(cov1));
+            w12 = - (0.5*myMult(myMult(myTrans(mean1),myInv(cov1)),mean1)) + log(probc1) - (0.5*log(abs(myMod(cov1))));
+            w21 = myMult(myTrans(mean2),myInv(cov2));
+            w22 = - (0.5*myMult(myMult(myTrans(mean2),myInv(cov2)),mean2)) + log(probc2) - (0.5*log(abs(myMod(cov2))));
+            w31 = myMult(myTrans(mean3),myInv(cov3));
+            w32 = - (0.5*myMult(myMult(myTrans(mean3),myInv(cov3)),mean3)) + log(probc3) - (0.5*log(abs(myMod(cov3))));
+            for i = 1:length(T1)
+                x = myTrans(T1(i,:)); 
+                x1=g3(x,cov1,w11,w12);
+                x2=g3(x,cov2,w21,w22);
+                x3=g3(x,cov3,w31,w32);
+                if(x1>x2 && x1>x3)
+                    %11
+                elseif(x2>x1 && x2>x3)
+                    12
+                elseif(x3>x1 && x3>x2)
+                    13
+                end
+            end
+            for i = 1:length(T2)
+                x = myTrans(T2(i,:)); 
+                x1=g3(x,cov1,w11,w12);
+                x2=g3(x,cov2,w21,w22);
+                x3=g3(x,cov3,w31,w32);
+                if(x1>x2 && x1>x3)
+                    21
+                elseif(x2>x1 && x2>x3)
+                    %22
+                elseif(x3>x1 && x3>x2)
+                    23
+                end
+            end
+            for i = 1:length(T3)
+                x = myTrans(T3(i,:)); 
+                x1=g3(x,cov1,w11,w12);
+                x2=g3(x,cov2,w21,w22);
+                x3=g3(x,cov3,w31,w32);
+                if(x1>x2 && x1>x3)
+                    31
+                elseif(x2>x1 && x2>x3)
+                    32
+                elseif(x3>x1 && x3>x2)
+                    %33
+                end
+            end
         end
     end
-    
-    
-        
-     %myCov(D1,myMean(D1))
-     %cov(D1)
 end    
